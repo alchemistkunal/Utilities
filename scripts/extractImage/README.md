45f233d605e66d6a7bc4ba1c65f03fdb6956b586
alchemistk <alchemisttech+github@pm.me>
2023-05-26 23:18:42 +0800

    Adding new project in progress - extract text from image

---
+++ scripts/extractImage/README.md
@@ -0,0 +1,9 @@
+pip install opencv-python pytesseract tesseract flask
+
+Install Tesseract OCR:
+
+    For Windows: Download and install the Tesseract OCR executable from the official GitHub repository (https://github.com/UB-Mannheim/tesseract/wiki).
+    For Linux: Install Tesseract OCR using the package manager of your distribution (apt, yum, etc.).
+    For macOS: Install Tesseract OCR using Homebrew (brew install tesseract).
+
+C:\Users\alche\AppData\Local\Programs\Tesseract-OCR
---
+++ scripts/extractImage/extract.py
@@ -0,0 +1,49 @@
+import pytesseract
+from flask import Flask, jsonify, request
+import os
+import datetime
+
+from PIL import Image
+app = Flask(__name__)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\alche\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+
+def extract_text(filePath: str)-> str:
+    # Open the image file
+    image = Image.open(filePath)
+
+    # Perform OCR and extract text from the image
+    return pytesseract.image_to_string(image).replace('\n', ' ')
+
+def run_service_logic():
+    folder_path = r"C:\Users\alche\Pictures\Screenshots" # Replace with the actual folder path
+    # Get a list of files in the folder
+    files = os.listdir(folder_path)
+    # Sort the files based on their creation time
+    sorted_files = sorted(files, key=lambda x: os.path.getctime(os.path.join(folder_path, x)), reverse=True)
+    # Get the top 15 files
+    top_files = sorted_files[:15]
+    # Print the top 15 files
+    response = {}
+    for file_name in top_files:
+        file_path = os.path.join(folder_path, file_name)
+        creation_time = datetime.datetime.fromtimestamp(os.path.getctime(file_path))
+        # print(f"{file_name} (Created: {creation_time})")
+        response[file_path] = extract_text(filePath=file_path)
+    return response
+
+@app.route('/', methods=['GET'])
+def fetch_application_list():
+    response_api = run_service_logic()
+    # response_api = ['AmEx','Amazon']
+    return response_api, 200
+
+
+@app.route('/api/health-status', methods=['GET'])
+def health_check():
+    return jsonify({"healthly": True, "service":"notion_integration"}), 200
+
+if __name__ == '__main__':
+    
+    app.run( port=4005)
+
